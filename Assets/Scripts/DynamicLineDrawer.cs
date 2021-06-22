c066192a017736d8e5a286ea0f5ea86a09526329
Julia Gammel <etopirog@mail.ru>
2021-06-22 04:04:48 +0300

    Построение линии по удержанию нажатия
    Упрощенный вариант

--- Assets/Scripts/DynamicLineDrawer.cs
+++ Assets/Scripts/DynamicLineDrawer.cs
@@ -10,7 +10,6 @@ public class DynamicLineDrawer : MonoBehaviour, IPointerDownHandler, IPointerUpH
     [SerializeField] private float offset = 1f;
     private float currentOffset = 0f;
     private Vector2 clickPoint = new Vector2(0, 0);
-    private float A = 0f, B = 0f, C = 0f; //коэффициенты линейного уравнения
 
     void Start()
     {
@@ -24,13 +23,8 @@ public class DynamicLineDrawer : MonoBehaviour, IPointerDownHandler, IPointerUpH
 
     public virtual void OnDrag(PointerEventData eventData)
     {
-        //Получаем координаты клика/тапа
-        //Debug.Log(eventData.position);
         currentOffset = offset;
         clickPoint = Camera.main.ScreenToWorldPoint(eventData.position);
-        //Debug.Log(clickPoint);
-
-        calculateLinearFuncCoefs(staticPoint.position, clickPoint);
 
     }
     public void OnPointerUp(PointerEventData eventData)
@@ -39,64 +33,8 @@ public class DynamicLineDrawer : MonoBehaviour, IPointerDownHandler, IPointerUpH
         clickPoint = staticPoint.position;
     }
 
-    private void calculateLinearFuncCoefs(Vector2 point1, Vector2 point2)
-    {
-        float x1 = point1.x;
-        float x2 = point2.x;
-
-        float y1 = point1.y;
-        float y2 = point2.y;
-
-        A = y1 - y2;
-        B = x2 - x1;
-        C = x1 * y2 - x2 * y1;
-    }
-
-    private float getLinearFuncY(float x)
-    {
-        if (B == 0) return 0;
-        return (-A * x - C) / B;
-    }
-
-    private float getLinearFuncX(float y)
-    {
-        if (A == 0) return 0;
-        return (-B * y - C) / A;
-    }
-
     private void OnDrawGizmos()
     {
-        
-        float Cy = staticPoint.position.y + currentOffset * direction(clickPoint, staticPoint);
-        //координаты новой точки = координаты центральной + смещение с учетом направления
-        float Cx = getLinearFuncX(Cy);
-        if (Cy == staticPoint.position.y) //есть проблема с начальной отрисовкой
-            Cx = staticPoint.position.x;
-        //Здесь получается ограничительный квадрат, проверяем, выходит ли за него точка
-            if (Cx >= (staticPoint.position.x + offset) //случай когда точка правее offset
-            || Cx <= (staticPoint.position.x - offset)) //случай когда точка левее -offset
-        {
-            // здесь мы проверили, входим ли мы в отрезок [x - offset, x + offset]
-            // рассмотрим случай "непринадлежности"
-            if (Cx <= (staticPoint.position.x - offset))
-            //В случае выхода за левую границу берем staticPoint.position.x - offset
-            {
-                Cx = staticPoint.position.x - offset;
-                Cy = getLinearFuncY(Cx);
-            }
-            else
-            {
-                Cx = staticPoint.position.x + offset;
-                Cy = getLinearFuncY(Cx);
-            }
-        }
-
-        Vector3 resultPoint = new Vector3(Cx, Cy, 0f);
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(staticPoint.position, 0.1f);
-        Gizmos.DrawLine(clickPoint, resultPoint);
-
-        /* правильная логика, заменяющая весь код выше и ниже
         //Выпускаем луч из точки касания в нашу "центральную" точку
         Ray ray = new Ray(staticPoint.position, new Vector2(staticPoint.position.x, staticPoint.position.y) - clickPoint); //1 параметр - начало луча, т.А, 2 пар-р - направление (B - A)
         //Получаем конец отрезка, который от центр.точки находится на расстоянии offset
@@ -105,16 +43,6 @@ public class DynamicLineDrawer : MonoBehaviour, IPointerDownHandler, IPointerUpH
         //Gizmos.DrawLine(clickPoint, resultPoint);
         Gizmos.DrawLine(clickPoint, destination);
         Gizmos.DrawSphere(staticPoint.position, 0.1f);
-        */
     }
-
-    private int direction(Vector2 point, Transform parentPoint)
-    {
-        Vector2 relativeCoordinate = parentPoint.InverseTransformPoint(point); //получаем относительные координаты
-        if (relativeCoordinate.y <= 0) 
-            return 1; //если тапаем снизу, то строим отрезок вверх
-        else return -1; //и соответственно в противоположном случае направление идет вниз
-    }
-
 
 }
